a5b1f36e19b65a722490007a6795d4b74f3a8453
igrios1979 <ignarios1@hotmail.com>
2024-03-06 10:27:31 -0300

    Este es el commit de hibernate

--- src/main/java/org/igrios/hibernate/appp/entity/Cliente.java
+++ src/main/java/org/igrios/hibernate/appp/entity/Cliente.java
@@ -1,2 +1,7 @@
-package org.igrios.hibernate.appp.entity;public class Cliente {
+package org.igrios.hibernate.appp.entity;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Cliente {
+
 }
