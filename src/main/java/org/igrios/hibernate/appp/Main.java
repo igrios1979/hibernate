89f2a323cf6413e70c8632b2b3a100c98917305d
igrios1979 <ignarios1@hotmail.com>
2024-03-06 09:39:48 -0300

    first commit

---
+++ src/main/java/org/igrios/hibernate/appp/Main.java
@@ -0,0 +1,7 @@
+package org.igrios.hibernate.appp;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/org/igrios/hibernate/appp/entity/Cliente.java
@@ -0,0 +1,2 @@
+package org.igrios.hibernate.appp.entity;public class Cliente {
+}
